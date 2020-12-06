84dcb39eae4f194f52323b6056db1d2cf50e3ff6
Yerkors <50750246+Yerkors@users.noreply.github.com>
2020-12-06 19:17:36 -0300

    Configuración FireBase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './Header/header/header.component';
-import { FooterComponent } from './Footer/footer/footer.component';
-import { HomeComponent } from './Body/Home/home/home.component';
-import { RegisterComponent } from './Body/Register/register/register.component';
-import { LoginComponent } from './Body/Login/login/login.component';
-import { ClickDirectiveDirective } from './Directives/click-directive.directive';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./Header/header/header.component";
+import { FooterComponent } from "./Footer/footer/footer.component";
+import { HomeComponent } from "./Body/Home/home/home.component";
+import { RegisterComponent } from "./Body/Register/register/register.component";
+import { LoginComponent } from "./Body/Login/login/login.component";
+import { ClickDirectiveDirective } from "./Directives/click-directive.directive";
 
+// FIREBASE
+import { AngularFireModule } from "@angular/fire";
+import { AngularFireAuthModule } from "@angular/fire/auth";
+import { environment } from "src/environments/environment";
 
 @NgModule({
   declarations: [
@@ -19,13 +23,15 @@ import { ClickDirectiveDirective } from './Directives/click-directive.directive'
     HomeComponent,
     RegisterComponent,
     LoginComponent,
-    ClickDirectiveDirective
+    ClickDirectiveDirective,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireAuthModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
