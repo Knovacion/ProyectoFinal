b360269940f3ec1c9f3b858102c353639bda75d8
Yerkors <50750246+Yerkors@users.noreply.github.com>
2020-12-06 18:21:30 -0300

    Credenciales FireBase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyCFK3wsj56x_0cX9Dz8B-J9oKHUL-0Oy1Q",
+    authDomain: "proyectofinalscalab-5e233.firebaseapp.com",
+    projectId: "proyectofinalscalab-5e233",
+    storageBucket: "proyectofinalscalab-5e233.appspot.com",
+    messagingSenderId: "544443157125",
+    appId: "1:544443157125:web:bd6748f3dac05ce340f7ec",
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyCFK3wsj56x_0cX9Dz8B-J9oKHUL-0Oy1Q",
+    authDomain: "proyectofinalscalab-5e233.firebaseapp.com",
+    projectId: "proyectofinalscalab-5e233",
+    storageBucket: "proyectofinalscalab-5e233.appspot.com",
+    messagingSenderId: "544443157125",
+    appId: "1:544443157125:web:bd6748f3dac05ce340f7ec",
+  }
 };
 
 /*
