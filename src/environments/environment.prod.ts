1236242497c207193edead0853d82d25acf64c9d
Michael Vargas <m.vargas.roa@gmail.com>
2020-12-06 18:28:18 -0300

    Credenciales MV

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -7,5 +7,14 @@ export const environment = {
     storageBucket: "proyectofinalscalab-5e233.appspot.com",
     messagingSenderId: "544443157125",
     appId: "1:544443157125:web:bd6748f3dac05ce340f7ec",
+   //Credencuial M
+    // firebaseConfig : {
+    //   apiKey: "AIzaSyCNIpsD6JsX9KjRevS8uTI9xthGmckYkjk",
+    //   authDomain: "proyectofinalscalab-12300.firebaseapp.com",
+    //   projectId: "proyectofinalscalab-12300",
+    //   storageBucket: "proyectofinalscalab-12300.appspot.com",
+    //   messagingSenderId: "840085594837",
+    //   appId: "1:840085594837:web:a249f49497241f8240385c",
+    //   measurementId: "G-GL1YQLRHXV"
   }
 };
